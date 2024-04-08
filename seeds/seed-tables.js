7a4028f4dbb7ed1ed16009d03177bec1297b7969
BilalHassen <bilalhassen097@gmail.com>
2024-04-08 13:11:59 -0400

    seed product_details data

--- seeds/seed-tables.js
+++ seeds/seed-tables.js
@@ -3,10 +3,11 @@
  * @returns { Promise<void> }
  */
 const products = require("../seed-data/products");
-
+const products_details = require("../seed-data/products-details");
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex("products_details").del();
   await knex("products").del();
   await knex("products").insert(products);
+  await knex("products_details").insert(products_details);
 };
