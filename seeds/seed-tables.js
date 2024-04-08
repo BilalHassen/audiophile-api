7e8c2b7fcebeba8a67e8cc686c66045de0a43780
BilalHassen <bilalhassen097@gmail.com>
2024-04-08 12:48:30 -0400

    remove code from seed-tables file

--- seeds/seed-tables.js
+++ seeds/seed-tables.js
@@ -1,13 +1,10 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+const products = require("../seed-data/products");
+
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+  await knex("products").del();
 };
