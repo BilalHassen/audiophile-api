0d9f16f2090b185bf817047b1b96edba7042389f
BilalHassen <bilalhassen097@gmail.com>
2024-04-08 12:49:07 -0400

    add default data to products seed-data file

---
+++ seed-data/products.js
@@ -0,0 +1,56 @@
+module.exports = [
+  {
+    id: 1,
+    name: "YX1 Wireless Earphones",
+    description:
+      "Tailor your listening experience with bespoke dynamic drivers from the new YX1 Wireless Earphones. Enjoy incredible high-fidelity sound even in noisy environments with its active noise cancellation feature.",
+    category: "earphones",
+    is_new: "true", // Convert to string representing boolean value
+    price: 599,
+  },
+  {
+    id: 2,
+    name: "XX59-Headphones",
+    description:
+      "Enjoy your audio almost anywhere and customize it to your specific tastes with the XX59 headphones. The stylish yet durable versatile wireless headset is a brilliant companion at home or on the move",
+    category: "headphones",
+    is_new: "false", // Convert to string representing boolean value
+    price: 899,
+  },
+  {
+    id: 3,
+    name: "XX99 Mark I Headphones",
+    description:
+      "As the gold standard for headphones, the classic XX99 Mark I offers detailed and accurate audio reproduction for audiophiles, mixing engineers, and music aficionados alike in studios and on the go.",
+    category: "headphones",
+    is_new: "false", // Convert to string representing boolean value
+    price: 1750,
+  },
+  {
+    id: 4,
+    name: "XX99 Mark II Headphones",
+    description:
+      "The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.",
+    category: "headphones",
+    is_new: "true", // Convert to string representing boolean value
+    price: 2999,
+  },
+  {
+    id: 5,
+    name: "ZX7 Speaker",
+    description:
+      "Stream high quality sound wirelessly with minimal to no loss. The ZX7 speaker uses high-end audiophile components that represents the top of the line powered speakers for home or studio use.",
+    category: "speakers",
+    is_new: "false", // Convert to string representing boolean value
+    price: 3500,
+  },
+  {
+    id: 6,
+    name: "ZX9 Speaker",
+    description:
+      "Upgrade your sound system with the all new ZX9 active speaker. It’s a bookshelf speaker system that offers truly wireless connectivity -- creating new possibilities for more pleasing and practical audio setups.",
+    category: "speakers",
+    is_new: "true", // Convert to string representing boolean value
+    price: 4500,
+  },
+];
