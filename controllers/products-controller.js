3c37621ac86576f6f47e0707ede685f91d1d17a7
BilalHassen <bilalhassen097@gmail.com>
2024-04-12 12:16:12 -0400

    add comments to getHeadphones query

--- controllers/products-controller.js
+++ controllers/products-controller.js
@@ -1,12 +1,18 @@
 const knex = require("knex")(require("../knexfile"));
 
 const getHeadphones = (req, res) => {
+  // Query the "products" table
   knex("products")
+    // Filter rows where the "category" column equals "headphones"
     .where("category", "headphones")
+    // Select specific columns for the result
     .select("name", "description", "url_mobile", "url_tablet", "url_desktop")
+    // Handle the result
     .then((productData) => {
+      // Send the queried data as JSON response
       res.json(productData);
     })
+    // Handle errors, if any
     .catch((err) => {
       console.log(err);
     });
