3797fec939a35bf15fa60763baf21d0070c2c139
BilalHassen <bilalhassen097@gmail.com>
2024-04-29 14:02:09 -0400

    add logic for getting speakers data for speakers page

--- controllers/products-controller.js
+++ controllers/products-controller.js
@@ -18,11 +18,35 @@ const getHeadphones = (req, res) => {
     .then((data) => {
       res.json(data);
     })
-    .catch((err) => {
+    .catch(() => {
+      res.status(500).json({ err: "Internal Server Error" });
+    });
+};
+
+const getSpeakers = (req, res) => {
+  knex("products")
+    .join("products_images", "products.id", "products_images.product_id")
+    // the category in the products table is = headphones
+    .where("category", "speakers")
+    .where("products_images.type", "categoryImage")
+    .select(
+      "products.id",
+      "products.name",
+      "products.description",
+      "products_images.url_mobile",
+      "products_images.url_tablet",
+      "products_images.url_desktop"
+    )
+    .then((data) => {
+      console.log({ data });
+      res.json(data);
+    })
+    .catch(() => {
       res.status(500).json({ err: "Internal Server Error" });
     });
 };
 
 module.exports = {
   getHeadphones,
+  getSpeakers,
 };
--- routes/products.js
+++ routes/products.js
@@ -2,5 +2,5 @@ const router = require("express").Router();
 const productController = require("../controllers/products-controller");
 
 router.route("/headphones").get(productController.getHeadphones);
-
+router.route("/speakers").get(productController.getSpeakers);
 module.exports = router;
