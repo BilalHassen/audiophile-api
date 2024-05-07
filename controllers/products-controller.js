976647026cc43bf02430fbc26552add4537dda84
BilalHassen <bilalhassen097@gmail.com>
2024-05-07 14:01:29 -0400

    wrote query for gettin product by id and fix seedfile id's for product with id: 6

--- controllers/products-controller.js
+++ controllers/products-controller.js
@@ -67,8 +67,58 @@ const getEarphones = (req, res) => {
     });
 };
 
+const getProductById = (req, res) => {
+  let productID = req.params.id;
+  let defaultData = null;
+
+  knex("products")
+    .where({ "products.id": productID })
+    .join("products_details", "products.id", "products_details.product_id")
+    .select(
+      "products.id",
+      "products.name",
+      "products.description", // Corrected column name
+      "products.price",
+      "products_details.features",
+      "products_details.includes"
+    )
+
+    .then((data) => {
+      defaultData = data;
+      if (defaultData.length === 0) {
+        throw new Error("Product Not Found");
+      }
+
+      return knex("products_images")
+        .where({ "products_images.product_id": productID })
+        .whereIn("type", ["gallery_first", "gallery_second", "gallery_third"])
+        .select(
+          "products_images.type",
+          "products_images.product_id",
+          "products_images.url_mobile",
+          "products_images.url_tablet",
+          "products_images.url_desktop"
+        );
+    })
+    .then((imageData) => {
+      const productData = {
+        data: defaultData,
+        imageData: imageData,
+      };
+      res.json(productData);
+    })
+
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: "internal server error, Product Not Found" });
+    });
+};
+
 module.exports = {
   getHeadphones,
   getSpeakers,
   getEarphones,
+  getProductById,
 };
--- routes/products.js
+++ routes/products.js
@@ -4,4 +4,6 @@ const productController = require("../controllers/products-controller");
 router.route("/headphones").get(productController.getHeadphones);
 router.route("/speakers").get(productController.getSpeakers);
 router.route("/earphones").get(productController.getEarphones);
+
+router.route("/:id").get(productController.getProductById);
 module.exports = router;
