6d5cfa6bb36e3bfbc8e98bbe5aeaf99fce934104
BilalHassen <bilalhassen097@gmail.com>
2024-05-16 13:07:58 -0400

    fix related_products seed file data to get the correct related items for each product

--- seed-data/related_products.js
+++ seed-data/related_products.js
@@ -1,6 +1,7 @@
 const baseURL = process.env.PORT || "http://localhost:5050";
 
 module.exports = [
+  // related products for yx1 earphones
   {
     id: 1,
     product_id: 1,
@@ -19,8 +20,39 @@ module.exports = [
     tablet_url: `${baseURL}/assets/shared/tablet/image-xx59-headphones.jpg`,
     desktop_url: `${baseURL}/assets/shared/desktop/image-xx59-headphones.jpg`,
   },
+
   {
     id: 3,
+    product_id: 1,
+    slug: "zx9-speaker",
+    name: "ZX9 Speaker",
+    mobile_url: `${baseURL}/assets/shared/mobile/image-zx9-speaker.jpg`,
+    tablet_url: `${baseURL}/assets/shared/tablet/image-zx9-speaker.jpg`,
+    desktop_url: `${baseURL}/assets/shared/desktop/image-zx9-speaker.jpg`,
+  },
+
+  // related products for mark II headphones
+  {
+    id: 4,
+    product_id: 2,
+    slug: "xx99-mark-one-headphones",
+    name: "XX99 Mark I",
+    mobile_url: `${baseURL}/assets/shared/mobile/image-xx99-mark-one-headphones.jpg`,
+    tablet_url: `${baseURL}/assets/shared/tablet/image-xx99-mark-one-headphones.jpg`,
+    desktop_url: `${baseURL}/assets/shared/desktop/image-xx99-mark-one-headphones.jpg`,
+  },
+  {
+    id: 5,
+    product_id: 2,
+    slug: "xx59-headphones",
+    name: "XX59",
+    mobile_url: `${baseURL}/assets/shared/mobile/image-xx59-headphones.jpg`,
+    tablet_url: `${baseURL}/assets/shared/tablet/image-xx59-headphones.jpg`,
+    desktop_url: `${baseURL}/assets/shared/desktop/image-xx59-headphones.jpg`,
+  },
+
+  {
+    id: 6,
     product_id: 2,
     slug: "zx9-speaker",
     name: "ZX9 Speaker",
@@ -28,8 +60,10 @@ module.exports = [
     tablet_url: `${baseURL}/assets/shared/tablet/image-zx9-speaker.jpg`,
     desktop_url: `${baseURL}/assets/shared/desktop/image-zx9-speaker.jpg`,
   },
+
+  // related products for mark I headphones
   {
-    id: 4,
+    id: 7,
     product_id: 3,
     slug: "xx99-mark-two-headphones",
     name: "XX99 Mark II",
@@ -38,35 +72,8 @@ module.exports = [
     desktop_url: `${baseURL}/assets/shared/desktop/image-xx99-mark-two-headphones.jpg`,
   },
   {
-    id: 5,
-    product_id: 3,
-    slug: "xx59-headphones",
-    name: "XX59",
-    mobile_url: `${baseURL}/assets/shared/mobile/image-xx59-headphones.jpg`,
-    tablet_url: `${baseURL}/assets/shared/tablet/image-xx59-headphones.jpg`,
-    desktop_url: `${baseURL}/assets/shared/desktop/image-xx59-headphones.jpg`,
-  },
-  {
-    id: 6,
-    product_id: 3,
-    slug: "zx9-speaker",
-    name: "ZX9 Speaker",
-    mobile_url: `${baseURL}/assets/shared/mobile/image-zx9-speaker.jpg`,
-    tablet_url: `${baseURL}/assets/shared/tablet/image-zx9-speaker.jpg`,
-    desktop_url: `${baseURL}/assets/shared/desktop/image-zx9-speaker.jpg`,
-  },
-  {
-    id: 7,
-    product_id: 4,
-    slug: "xx99-mark-one-headphones",
-    name: "XX99 Mark I",
-    mobile_url: `${baseURL}/assets/shared/mobile/image-xx99-mark-one-headphones.jpg`,
-    tablet_url: `${baseURL}/assets/shared/tablet/image-xx99-mark-one-headphones.jpg`,
-    desktop_url: `${baseURL}/assets/shared/desktop/image-xx99-mark-one-headphones.jpg`,
-  },
-  {
     id: 8,
-    product_id: 4,
+    product_id: 3,
     slug: "xx59-headphones",
     name: "XX59",
     mobile_url: `${baseURL}/assets/shared/mobile/image-xx59-headphones.jpg`,
@@ -75,25 +82,28 @@ module.exports = [
   },
   {
     id: 9,
-    product_id: 4,
+    product_id: 3,
     slug: "zx9-speaker",
     name: "ZX9 Speaker",
     mobile_url: `${baseURL}/assets/shared/mobile/image-zx9-speaker.jpg`,
     tablet_url: `${baseURL}/assets/shared/tablet/image-zx9-speaker.jpg`,
     desktop_url: `${baseURL}/assets/shared/desktop/image-zx9-speaker.jpg`,
   },
+
+  // related products for xx59 headphones
   {
     id: 10,
-    product_id: 5,
-    slug: "zx7-speaker",
-    name: "ZX7 Speaker",
-    mobile_url: `${baseURL}/assets/shared/mobile/image-zx7-speaker.jpg`,
-    tablet_url: `${baseURL}/assets/shared/tablet/image-zx7-speaker.jpg`,
-    desktop_url: `${baseURL}/assets/shared/desktop/image-zx7-speaker.jpg`,
+    product_id: 4,
+    slug: "xx99-mark-two-headphones",
+    name: "XX99 Mark II",
+    mobile_url: `${baseURL}/assets/shared/mobile/image-xx99-mark-two-headphones.jpg`,
+    tablet_url: `${baseURL}/assets/shared/tablet/image-xx99-mark-two-headphones.jpg`,
+    desktop_url: `${baseURL}/assets/shared/desktop/image-xx99-mark-two-headphones.jpg`,
   },
+
   {
     id: 11,
-    product_id: 5,
+    product_id: 4,
     slug: "xx99-mark-one-headphones",
     name: "XX99 Mark I",
     mobile_url: `${baseURL}/assets/shared/mobile/image-xx99-mark-one-headphones.jpg`,
@@ -102,25 +112,27 @@ module.exports = [
   },
   {
     id: 12,
-    product_id: 5,
-    slug: "xx59-headphones",
-    name: "XX59",
-    mobile_url: `${baseURL}/assets/shared/mobile/image-xx59-headphones.jpg`,
-    tablet_url: `${baseURL}/assets/shared/tablet/image-xx59-headphones.jpg`,
-    desktop_url: `${baseURL}/assets/shared/desktop/image-xx59-headphones.jpg`,
+    product_id: 4,
+    slug: "zx9-speaker",
+    name: "ZX9 Speaker",
+    mobile_url: `${baseURL}/assets/shared/mobile/image-zx9-speaker.jpg`,
+    tablet_url: `${baseURL}/assets/shared/tablet/image-zx9-speaker.jpg`,
+    desktop_url: `${baseURL}/assets/shared/desktop/image-zx9-speaker.jpg`,
   },
+  // related products for zx7
+
   {
     id: 13,
-    product_id: 6,
-    slug: "zx7-speaker",
-    name: "ZX7 Speaker",
-    mobile_url: `${baseURL}/assets/shared/mobile/image-zx7-speaker.jpg`,
-    tablet_url: `${baseURL}/assets/shared/tablet/image-zx7-speaker.jpg`,
-    desktop_url: `${baseURL}/assets/shared/desktop/image-zx7-speaker.jpg`,
+    product_id: 5,
+    slug: "zx9-speaker",
+    name: "ZX9 Speaker",
+    mobile_url: `${baseURL}/assets/shared/mobile/image-zx9-speaker.jpg`,
+    tablet_url: `${baseURL}/assets/shared/tablet/image-zx9-speaker.jpg`,
+    desktop_url: `${baseURL}/assets/shared/desktop/image-zx9-speaker.jpg`,
   },
   {
     id: 14,
-    product_id: 6,
+    product_id: 5,
     slug: "xx99-mark-one-headphones",
     name: "XX99 Mark I",
     mobile_url: `${baseURL}/assets/shared/mobile/image-xx99-mark-one-headphones.jpg`,
@@ -129,6 +141,35 @@ module.exports = [
   },
   {
     id: 15,
+    product_id: 5,
+    slug: "xx59-headphones",
+    name: "XX59",
+    mobile_url: `${baseURL}/assets/shared/mobile/image-xx59-headphones.jpg`,
+    tablet_url: `${baseURL}/assets/shared/tablet/image-xx59-headphones.jpg`,
+    desktop_url: `${baseURL}/assets/shared/desktop/image-xx59-headphones.jpg`,
+  },
+
+  // related products for zx9 speaker
+  {
+    id: 16,
+    product_id: 6,
+    slug: "zx7-speaker",
+    name: "ZX7 Speaker",
+    mobile_url: `${baseURL}/assets/shared/mobile/image-zx7-speaker.jpg`,
+    tablet_url: `${baseURL}/assets/shared/tablet/image-zx7-speaker.jpg`,
+    desktop_url: `${baseURL}/assets/shared/desktop/image-zx7-speaker.jpg`,
+  },
+  {
+    id: 17,
+    product_id: 6,
+    slug: "xx99-mark-one-headphones",
+    name: "XX99 Mark I",
+    mobile_url: `${baseURL}/assets/shared/mobile/image-xx99-mark-one-headphones.jpg`,
+    tablet_url: `${baseURL}/assets/shared/tablet/image-xx99-mark-one-headphones.jpg`,
+    desktop_url: `${baseURL}/assets/shared/desktop/image-xx99-mark-one-headphones.jpg`,
+  },
+  {
+    id: 18,
     product_id: 6,
     slug: "xx59-headphones",
     name: "XX59",
