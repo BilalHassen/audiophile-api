273003a90e5192201ff6e8430fa8b167ff7f7425
BilalHassen <bilalhassen097@gmail.com>
2024-05-27 17:11:52 -0400

    add cart and cart_items tables to database

---
+++ migrations/20240527162222_create_cart_and_cart_items_tables.js
@@ -0,0 +1,35 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  // table for each individual cart
+  return knex.schema
+    .createTable("cart", (table) => {
+      table.increments("id").unsigned().primary();
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+    })
+    .createTable("cart_items", (table) => {
+      table.increments("id").unsigned().primary();
+      table.integer("cart_id").notNullable();
+      table
+        .foreign("cart_id")
+        .references("id")
+        .inTable("cart")
+        .onDelete("CASCADE");
+
+      table.integer("product_id").notNullable();
+      table.integer("quantity").notNullable();
+      table.decimal("price", 10, 2).notNullable();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("cart_items").then(() => {
+    return knex.schema.dropTableIfExists("cart");
+  });
+};
