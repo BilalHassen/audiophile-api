fb2777557e27b140565bf275fe5f2bd4595f32e0
BilalHassen <bilalhassen097@gmail.com>
2024-05-28 16:42:26 -0400

    fix addItem function

--- controllers/carts-controllers.js
+++ controllers/carts-controllers.js
@@ -5,17 +5,23 @@ const addItem = (req, res) => {
   // destruct the req.body contents
   const { id, product_id, quantity, price, cart_id } = req.body;
 
-  // check if the id exists in the cart table
+  console.log({ quantity });
+  // query the cart table to see if the cart exists
   knex("cart")
     .where({ id: cart_id })
+    // select the first row returned even if it returns multiple rows
     .first()
-    .then((existingCart) => {
-      // if not add it
-      if (!existingCart) {
+    // handle the returned promise from the above query
+    .then((exisitngCart) => {
+      console.log(exisitngCart);
+      // if the cart table doesnt have the cart_id insert it
+      // this code will only run if the cart_id isnt in the cart table
+      if (!exisitngCart) {
         return knex("cart").insert({ id: cart_id });
       }
     })
     .then(() => {
+      // check if the cart_items related to the cart_id exists
       return knex("cart_items")
         .where({
           cart_id: cart_id,
@@ -23,27 +29,23 @@ const addItem = (req, res) => {
         })
         .first();
     })
+    // exisitngItem is object returned from above promise
     .then((existingItem) => {
+      // check if the item exists if it does update it
       if (existingItem) {
         return knex("cart_items")
           .where({ cart_id: cart_id, product_id: product_id })
           .update({
-            quantity: knex.raw("?", [quantity]),
-            price: price * quantity,
+            // take the existingItem quantity and add the value of the new quantity from the req
+            quantity: existingItem.quantity + quantity,
+            price: price * (existingItem.quantity + quantity),
           });
-      } else if (quantity > 1) {
-        return knex("cart_items").insert({
-          cart_id: cart_id,
-          product_id: product_id,
-          quantity: knex.raw("?", [quantity]),
-          price: price * quantity,
-        });
-      } else if (quantity <= 1) {
+      } else {
         return knex("cart_items").insert({
           cart_id: cart_id,
           product_id: product_id,
           quantity: quantity,
-          price: price,
+          price: price * quantity,
         });
       }
     });
